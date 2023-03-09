e0c19d2d34baca797bd15a100de687597a634305
kengo <mathematics9973@gmail.com>
2023-03-09 18:38:48 +0900

    setting of routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :blogs
-  resources :users
+  root 'sessions#new' 
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+  resources :users, only: %i[new create edit update destroy show] 
+  resources :sessions, only: %i[new create destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
