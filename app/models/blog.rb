25a56cb4b3f82882251f2d493c7c7396494769e9
kasumi sogabe <ksm17sgb@gmail.com>
2023-03-10 15:58:03 +0900

    バリデーション追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 1, maximum: 50 }
+  validates :content, presence: true, length: { minimum: 1, maximum: 200 }
 end
